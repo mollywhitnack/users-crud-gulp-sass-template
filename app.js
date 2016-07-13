c12a62d13eec7de46c05554c24ce055ff401f911
mollywhitnack <mollywhitnack@gmail.com>
2016-07-13 09:30:08 -0700

    dsd

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use((req, res, next) => {
 app.use('/api', require('./routes/api'));
 
 app.get('/', (req, res) => {
-  res.render('index', {title: 'Users/Crud/Gulp/Sass Template'});
+  res.render('index', {title: 'Template'});
 });
 
 // catch 404 and forward to error handler
